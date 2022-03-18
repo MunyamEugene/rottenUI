0d5670ed8a03e900c82d2e33b87d6e7a3afffcee
MunyamEugene <munyampundueugene3@gmail.com>
2022-03-18 09:36:13 +0200

    change backend url

--- src/components/authComponent/authComponent.js
+++ src/components/authComponent/authComponent.js
@@ -15,7 +15,7 @@ const AuthComponent = ({ login, logout, state }) => {
     <div className="auth">
       {given_name}
       {(email_verified) ? <img src={`${picture}`} height="100" width="100" alt={given_name} /> : ''}
-      {(!email_verified) ? <button className="authbtn"><a href="http://localhost:5000/api/auth/google">Login with google</a></button> : ''}
+      {(!email_verified) ? <button className="authbtn"><a href="https://rottenback.herokuapp.com/api/auth/google">Login with google</a></button> : ''}
       {(email_verified) ? <button className="authbtn" onClick={handleOnLogout}>Loout</button> : ''}
     </div>
   );
