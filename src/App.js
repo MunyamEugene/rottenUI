c654334deb5c8e1621d26be89ef8ee44be2cb535
MunyamEugene <munyampundueugene3@gmail.com>
2022-03-19 21:27:27 +0200

    add search option

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import AboutComponent from './components/authComponent/aboutComponent/aboutComonent';
 import AuthContainer from './components/authComponent/authContainer';
 import HotelContainer from './components/hotelComponet/hotelContainer';
+import VoteContainer from './components/voteComponent/voteContainer';
 import './index.css';
+import AboutComponent from './components/authComponent/aboutComponent/aboutComonent';
 
 const App = () => (
   <div className="app">
     <AuthContainer />
     <AboutComponent />
     <HotelContainer />
+    <VoteContainer />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/reducers/store';
 import './index.css';
-import VoteContainer from './components/voteComponent/voteContainer';
 
 ReactDOM.render(
   <Provider store={store}>
-    <VoteContainer />
     <App />
   </Provider>,
   document.getElementById('root'),
